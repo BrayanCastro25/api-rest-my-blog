6dbeccf95e4185e43fd540d2d8703ca567031d74
Brayan Castro <davidbarbosa0525@gmail.com>
2023-05-26 19:48:30 -0500

    the function for storing items in the database has been added

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,5 +1,7 @@
 const validator = require("validator");
 
+const Articulo = require("../models/Articles");
+
 const test = (req, res) => {
     return res.status(200).json({
         mensaje: "Soy una acción de prueba en mi controlador de artículos" 
@@ -24,7 +26,7 @@ const create = (req, res) => {
 
     // Validar datos
     try{
-        let validar_titulo = !validator.isEmpty(parameters.title) && validator.isLength(parameters.title, {min: 5, max: 15}); 
+        let validar_titulo = !validator.isEmpty(parameters.title) 
         let validar_contenido = !validator.isEmpty(parameters.content); 
 
         if(!validar_contenido || !validar_titulo){
@@ -38,15 +40,28 @@ const create = (req, res) => {
     }
 
     // Crear el objeto a guardar
+    const new_article = new Articulo(parameters);
 
     // Asignar valores a objeto basado en el modelo (manual o autómatico)
 
     // Guardar el articulo en la base de datos
-
-    return res.status(200).json({
-        mensaje: "Acción de guardar",
-        parameters
-    })
+    new_article.save()
+        .then((articleSaved) => {
+            console.log(articleSaved);
+            return res.status(200).json({
+                status: "success",
+                article: articleSaved,
+                message: "Article was saved successfully!!"
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+            return res.status(400).json({
+                status: "error",
+                message: "Article was not saved..."
+            })
+        })
+     
 }
 
 module.exports = {
